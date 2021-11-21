5d9da55e56675d04a14d98dbac6f5f7af82e7c37
AlexandraHockett <87495152+AlexandraHockett@users.noreply.github.com>
2021-11-21 13:56:33 +0000

    Imported old work done in CodeSandbox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
+import "./Weather.css";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <div className="App">
+        <Weather />
+      </div>
     </div>
   );
 }
